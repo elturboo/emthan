508ac862dedcb02a625f0bfdbc0abcfdabe8b300
Mahmoud Awad <m.awad@tamkeentech.sa>
2023-07-10 11:22:10 +0300

    update otp

--- js/main.js
+++ js/main.js
@@ -1,18 +1,35 @@
-$('.material-filter').owlCarousel({
-    loop:false,
-    margin:5,
-    nav:true,
-    rtl:true,
-    dots:false,
-    responsive:{
-        0:{
-            items:3
-        },
-        600:{
-            items:4
-        },
-        1000:{
-            items:7
-        }
+$(".material-filter").owlCarousel({
+  loop: false,
+  margin: 5,
+  nav: true,
+  rtl: true,
+  dots: false,
+  responsive: {
+    0: {
+      items: 3,
+    },
+    600: {
+      items: 4,
+    },
+    1000: {
+      items: 7,
+    },
+  },
+});
+
+var container = document.getElementsByClassName("otp-form")[0];
+container.onkeyup = function (e) {
+  var target = e.srcElement;
+  var maxLength = parseInt(target.attributes["maxlength"].value, 10);
+  var myLength = target.value.length;
+  if (myLength >= maxLength) {
+    var next = target;
+    while ((next = next.nextElementSibling)) {
+      if (next == null) break;
+      if (next.tagName.toLowerCase() == "input") {
+        next.focus();
+        break;
+      }
     }
-})
+  }
+};
